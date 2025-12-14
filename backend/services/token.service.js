c0b3140c1a2abd754bb93d9b306a11d15a4fe0a7
jenul-ferdinand <jenul15ferdinand@gmail.com>
2025-12-14 17:51:22 +1100

    fix: longer refresh token expiry

--- backend/services/token.service.js
+++ backend/services/token.service.js
@@ -2,14 +2,14 @@ const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 
 class TokenService {
-    static ACCESS_TOKEN_EXPIRY = 15 * 60 * 1000;
-    static REFRESH_TOKEN_EXPIRY = 7 * 24 * 60 * 60 * 1000;
+    static ACCESS_TOKEN_EXPIRY = 15 * 60 * 1000; // 15 minutes
+    static REFRESH_TOKEN_EXPIRY = 180 * 24 * 60 * 60 * 1000;
 
     static generateAccessToken(userId, isAdmin) {
         return jwt.sign(
             { id: userId, isAdmin },
             process.env.JWT_SECRET,
-            { expiresIn: '15m' },
+            { expiresIn: this.ACCESS_TOKEN_EXPIRY },
         );
     };
 
