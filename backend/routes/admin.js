f0642032bc806a34d6a05ccff1c90264ad905089
jenul-ferdinand <jenul15ferdinand@gmail.com>
2025-12-19 15:02:30 +1100

    add admin router with cache invalidation helper endpoint

---
+++ backend/routes/admin.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const CacheService = require('../services/redis.service.js');
+const { verifyAdmin } = require('../utils/verify_token.js');
+const router = express.Router();
+
+router.get('/invalidate-cache', async (req, res) => {
+  // #swagger.tags = ['Developer']
+  // #swagger.summary = 'Invalidate the cache'
+
+  try {
+    await CacheService.invalidate('*');
+
+    res.status(200).json({ message: 'Successfully invalidated all cache' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error when invalidating cache' });
+  }
+});
+
+module.exports = router;
