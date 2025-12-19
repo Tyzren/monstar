2b8c8622ac04da0f5511212e9eb17836d88b64f3
jenul-ferdinand <jenul15ferdinand@gmail.com>
2025-12-19 15:02:42 +1100

    add v2 popular units route

---
+++ backend/routes/v2/units.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const Unit = require('../../models/unit');
+const router = express.Router();
+const CacheService = require('../../services/redis.service');
+
+router.get('/popular', async (req, res) => {
+  // #swagger.tags = ['Units V2']
+  // #swagger.summary = 'Get 10 most popular units'
+
+  try {
+    const popularUnits = await CacheService.getOrSet(
+      'units:popular',
+      async () => {
+        const aggResults = await Unit.aggregate([
+          {
+            $addFields: {
+              reviewCount: { $size: '$reviews' },
+            },
+          },
+          {
+            $sort: { reviewCount: -1 },
+          },
+          {
+            $limit: 10,
+          },
+        ]);
+
+        return aggResults;
+      },
+      CacheService.POPULAR_UNITS_TTL
+    );
+
+    return res.status(200).json(popularUnits);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: 'An error occured while fetching popular units.' });
+  }
+});
+
+module.exports = router;
