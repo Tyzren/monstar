b09e3a0e903eb08f008d7343c6762a5031156229
jenul-ferdinand <jenul15ferdinand@gmail.com>
2025-12-25 21:52:46 +1100

    setup jest config for testing

---
+++ backend/jest.config.js
@@ -0,0 +1,27 @@
+const { _moduleAliases } = require('./package.json');
+
+/**
+ * Converts aliases into Jest regex
+ *
+ * E.g., "@models" -> "^@models/(.*)$" -> "<rootDir>/models/$1"
+ */
+const makeModuleNameMapper = (aliases) => {
+  const mapper = {};
+
+  for (const alias in aliases) {
+    const path = aliases[alias];
+
+    const aliasRegex = `^${alias}/(.*)$`;
+
+    const pathPattern = `<rootDir>/${path.replace(/^\.\//, '')}/$1`;
+
+    mapper[aliasRegex] = pathPattern;
+  }
+  return mapper;
+};
+
+module.exports = {
+  testEnvironment: 'node',
+  moduleNameMapper: makeModuleNameMapper(_moduleAliases),
+  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.js'],
+};
