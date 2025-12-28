769a55f45f1c32672c6748fa547d17d771c3e909
jenul-ferdinand <jenul15ferdinand@gmail.com>
2025-12-28 17:29:35 +1100

    add methods to unit service

--- backend/infra/services/unit.service.js
+++ backend/infra/services/unit.service.js
@@ -1,34 +1,76 @@
 const { getSortCriteria } = require('@constants/sortOptions');
 const UnitRepository = require('@infra/repositories/unit.repository');
 const { buildFilterQuery } = require('@infra/utilities/unitFilterHelpers');
-const Unit = require('@models/unit');
 
 class UnitService {
   /**
    * Get all units
    */
-  static getAllUnits = async () => {
-    return await Unit.find({}).populate('reviews');
+  static fetchAll = async () => {
+    return await UnitRepository.findAll();
   };
 
   /**
    * Get units filtered
    */
-  static getFilteredUnits = async (filterOptions) => {
+  static fetchPaginated = async (filterOptions) => {
     const { offset = 0, limit = 10, sort = 'Alphabetic' } = filterOptions;
 
     const query = buildFilterQuery(filterOptions);
     const sortCriteria = getSortCriteria(sort);
 
-    return await UnitRepository.queryPaginatedUnits(query, sortCriteria, offset, limit);
+    return await UnitRepository.findWithPagination(
+      query,
+      sortCriteria,
+      offset,
+      limit
+    );
   };
 
   /**
    * Get N most reviewed units
    */
-  static async getMostReviewedUnits(n = 10) {
-    return await UnitRepository.queryMostReviewedUnits(n);
-  }
+  static fetchMostReviewed = async (n = 10) => {
+    return await UnitRepository.findMostReviewedUnits(n);
+  };
+
+  /**
+   * Get a unit by unitcode
+   */
+  static fetchByCode = async (unitCode) => {
+    return await UnitRepository.findOneByUnitcode(unitCode);
+  };
+
+  /**
+   * Modify a unit
+   */
+  static modifyByUnitcode = async (unitCode, updateData) => {
+    const allowedFields = [
+      'name',
+      'description',
+      'avgOverallRating',
+      'avgContentRating',
+      'avgFacultyRating',
+      'avgRelevancyRating',
+    ];
+    const hasOnlyAllowedFields = Object.keys(updateData).every((key) =>
+      allowedFields.includes(key)
+    );
+    if (!hasOnlyAllowedFields)
+      throw new Error('Disallowed fields present in update data');
+    const unit = await UnitRepository.updateOneByUnitcode(unitCode, updateData);
+    if (!unit) throw new Error('Unit not found');
+    return unit;
+  };
+
+  /**
+   * Fetch all units that have the given unit as a prerequisite
+   */
+  static fetchUnitsRequiredBy = async (unitCode) => {
+    const unit = await UnitRepository.findOneByUnitCode(unitCode);
+    if (!unit) throw new Error('Unit not found');
+    return await UnitRepository.findRequiredBy(unitCode);
+  };
 }
 
 module.exports = UnitService;
