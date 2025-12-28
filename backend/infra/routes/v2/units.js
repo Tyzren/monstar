ca12221a484f43759f4a33283e8775019218d529
jenul-ferdinand <jenul15ferdinand@gmail.com>
2025-12-28 17:29:57 +1100

    add endpoints to units v2 router

--- backend/infra/routes/v2/units.js
+++ backend/infra/routes/v2/units.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const UnitController = require('@controllers/unit.controller');
+const adminMiddleware = require('@infra/middleware/admin.middleware');
 
 const router = express.Router();
 
@@ -8,14 +9,36 @@ router.get(
   '/',
   // #swagger.tags = ['Units V2']
   // #swagger.summary = 'Get all units'
-  UnitController.listAll
+  UnitController.getAll
 );
 
 router.get(
   '/popular',
   // #swagger.tags = ['Units V2']
   // #swagger.summary = 'Get 10 most popular units'
-  UnitController.listMostReviewed
+  UnitController.getMostReviewed
+);
+
+router.get(
+  '/filter',
+  // #swagger.tags = ['Units V2']
+  // #swagger.summary = 'Get paginated and filtered units'
+  UnitController.getPaginated
+);
+
+router.put(
+  '/update/:unitcode',
+  adminMiddleware,
+  // #swagger.tags = ['Units V2']
+  // #swagger.summary = 'Update a unit'
+  UnitController.updateByUnitcode
+);
+
+router.get(
+  '/:unitCode/required-by',
+  // #swagger.tags = ['Units V2']
+  // #swagger.description = 'Get all units that have the given unit as a prerequisite'
+  UnitController.getRequiredBy
 );
 
 module.exports = router;
