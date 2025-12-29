3b50603c1c181776f9dd3d771cc3dd63a302909b
jenul-ferdinand <jenul15ferdinand@gmail.com>
2025-12-29 15:55:45 +1100

    add methods to user repo

--- backend/infra/repositories/user.repository.js
+++ backend/infra/repositories/user.repository.js
@@ -9,6 +9,29 @@ class UserRepository {
   static async findById(userId) {
     return await User.findById(userId);
   }
+
+  /**
+   * Find a user by email or google id
+   */
+  static async findByEmailOrGoogleId(email, googleId) {
+    return await User.findOne({
+      $or: [{ email: email }, { googleId: googleId }]
+    });
+  }
+
+  /**
+   * Create a user
+   */
+  static async create(userData) {
+    const user = new User(userData);
+    return await user.save();
+  }
+
+  static async updateRefreshToken(user, hashedToken, expiry) {
+    user.refreshToken = hashedToken;
+    user.refreshTokenExpires = expiry;
+    return await user.save();
+  }
 }
 
 module.exports = UserRepository;
