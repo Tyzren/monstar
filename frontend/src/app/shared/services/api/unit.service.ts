94d015b504a82c3343dac930bc762bd8663b9b9f
jenul-ferdinand <jenul15ferdinand@gmail.com>
2025-12-30 00:05:12 +1100

    remove unused import

--- frontend/src/app/shared/services/api/unit.service.ts
+++ frontend/src/app/shared/services/api/unit.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Unit } from '../../models/unit.model';
 import { FilteredUnitsResponse } from '../../models/v2/unit.model';
 
 @Injectable({
