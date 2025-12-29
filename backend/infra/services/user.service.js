d70cc103cd2163f3d605acef295a47b32fc516f8
jenul-ferdinand <jenul15ferdinand@gmail.com>
2025-12-29 15:55:54 +1100

    create user service

---
+++ backend/infra/services/user.service.js
@@ -0,0 +1,77 @@
+const { OAuth2Client } = require('google-auth-library');
+
+const {
+  ConflictError,
+  DomainValidationError,
+} = require('@infra/utilities/errors');
+const TokenProvider = require('@providers/token.provider');
+const UserRepository = require('@repositories/user.repository');
+
+const googleClient = new OAuth2Client();
+
+class UserService {
+  static STUDENT_EMAIL_REGEX = /^[a-zA-Z]{4}\d{4}@student\.monash\.edu$/;
+  static STAFF_EMAIL_REGEX = /^[a-zA-Z]+\.[a-zA-Z]+@monash\.edu$/;
+
+  /**
+   * Authenticates a new or existing user with Google OAuth
+   *
+   * @param {String} idToken
+   * @returns {Object}
+   */
+  static googleAuthenticate = async (idToken) => {
+    const ticket = await googleClient.verifyIdToken({
+      idToken: idToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
+    const { email, name, picture, sub } = payload; // eslint-disable-line
+
+    const isStudentEmail = this.STUDENT_EMAIL_REGEX.test(email);
+    const isStaffEmail = this.STAFF_EMAIL_REGEX.test(email);
+    if (!isStudentEmail && !isStaffEmail) {
+      throw new DomainValidationError(
+        'Only students with a valid Monash email can log in.'
+      );
+    }
+
+    let user = await UserRepository.findByEmailOrGoogleId(email, sub);
+
+    if (!user) {
+      let authcate;
+      if (isStudentEmail) authcate = email.split('@')[0];
+      else if (isStaffEmail) authcate = email.split('.')[0];
+
+      user = await UserRepository.create({
+        email: email,
+        username: authcate,
+        profileImg: picture,
+        isGoogleUser: true,
+        googleID: sub,
+        verified: true,
+      });
+    }
+
+    if (!user.isGoogleUser) {
+      throw new ConflictError('Account already exists as non-Google account.');
+    }
+
+    const accessToken = TokenProvider.generateAccessToken(user._id, user.admin);
+    const refreshToken = TokenProvider.generateRefreshToken();
+    const hashedRefreshToken = TokenProvider.hashRefreshToken(refreshToken);
+    const refreshTokenExpiry = new Date(
+      Date.now() + TokenProvider.REFRESH_TOKEN_EXPIRY
+    );
+    await UserRepository.updateRefreshToken(
+      user,
+      hashedRefreshToken,
+      refreshTokenExpiry
+    );
+
+    return { accessToken, refreshToken, user };
+  };
+
+  
+}
+
+module.exports = UserService;
