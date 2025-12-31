b2f64cdfaca488a24be445b583ebe49b3c7f9807
jenul-ferdinand <jenul15ferdinand@gmail.com>
2025-12-31 19:27:53 +1100

    update artillery test to use test server port

--- backend/tests/performance/units.popular.test.js
+++ backend/tests/performance/units.popular.test.js
@@ -3,6 +3,7 @@ const { readFile: readFileAsync } = require('node:fs/promises');
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
+const { TEST_PORT } = require('./jest.setup');
 
 const execAsync = util.promisify(exec);
 
@@ -10,13 +11,14 @@ describe('Performance Smoke Tests', () => {
   const runArtillery = async (scriptName) => {
     const scriptPath = path.join(__dirname, scriptName + '.yml');
     const reportPath = path.join(__dirname, scriptName + '.report.json');
+    const testServerUrl = `http://localhost:${TEST_PORT}`;
 
     if (fs.existsSync(reportPath)) fs.unlinkSync(reportPath);
 
     console.log(`Running ${scriptName}`);
 
     try {
-      await execAsync(`npx artillery run -o "${reportPath}" "${scriptPath}"`);
+      await execAsync(`npx artillery run -t "${testServerUrl}" -o "${reportPath}" "${scriptPath}"`);
     } catch (err) {
       console.error(`❌ Artillery CRASHED for ${scriptName}`);
       console.error(err.stdout);
