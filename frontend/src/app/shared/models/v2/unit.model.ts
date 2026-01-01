c80f92e1c3b27be77074b708b0e38a91c929c2ce
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-01-01 16:18:03 +1100

    fe: add type for filter data

--- frontend/src/app/shared/models/v2/unit.model.ts
+++ frontend/src/app/shared/models/v2/unit.model.ts
@@ -41,6 +41,18 @@ export interface UnitData {
   aiOverview: AiOverview;
 }
 
+export interface FilterData {
+  offset: number,
+  limit: number,
+  search: string,
+  sort: string,
+  showReviewed: boolean,
+  showUnreviewed: boolean,
+  hideNoOfferings: boolean,
+  selectedFaculties: Array<string>,
+  selectedSemesters: Array<string>,
+  selectedCampuses: Array<string>,
+}
 export interface FilteredUnitsResponse {
   units: Array<UnitData>;
   total: number;
