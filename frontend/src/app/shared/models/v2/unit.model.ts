7452eedb299d4b3cfec91d7799013b6ecfdc69e0
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-01-03 20:32:32 +1100

    fe: update unit model

--- frontend/src/app/shared/models/v2/unit.model.ts
+++ frontend/src/app/shared/models/v2/unit.model.ts
@@ -1,50 +1,9 @@
-import { AiOverview } from '../ai-overview.model';
-import { IReview, IReviewAuthorPopulated } from './review.model';
+import { IUnit } from "./unit.schema";
 
 export enum UnitTag {
   MOST_REVIEWS = 'most-reviews',
   CONTROVERSIAL = 'controversial',
   WAM_BOOSTER = 'wam-booster',
-}
-export interface Requisites {
-  permission: boolean;
-  prerequisites: { NumReq: number; units: string[] }[];
-  corequisites: string[];
-  prohibitions: string[];
-  cpRequired: number;
-}
-export interface Offering {
-  location: string;
-  mode: string;
-  name: string;
-  period: string;
-  _id: string;
-}
-export interface IUnit {
-  _id: string;
-  unitCode: string;
-  name: string;
-  description?: string;
-  reviews: string[];
-  avgOverallRating: number;
-  avgRelevancyRating: number;
-  avgFacultyRating: number;
-  avgContentRating: number;
-  level: number;
-  creditPoints: number;
-  school: string;
-  academicOrg: string;
-  scaBand: number;
-  requisites: Requisites;
-  offerings: Offering[];
-  tags: UnitTag[];
-  aiOverview: AiOverview;
-}
-export interface IUnitPopulated extends Omit<IUnit, 'reviews'> {
-  reviews: IReview[];
-}
-export interface IUnitDeeplyPopulated extends Omit<IUnit, 'reviews'> {
-  reviews: IReviewAuthorPopulated[];
 }
 
 export interface FilterData {
