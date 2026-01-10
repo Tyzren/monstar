625ce4844539a0c358866b3cc6946752d7f9dcce
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-01-10 18:05:41 +1100

    fe: add input size to shiny-monstar-title

--- frontend/src/app/shared/ui/shiny-monstar-title/shiny-monstar-title.component.ts
+++ frontend/src/app/shared/ui/shiny-monstar-title/shiny-monstar-title.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'app-shiny-monstar-title',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './shiny-monstar-title.component.scss'
 })
 export class ShinyMonstarTitleComponent {
+  @Input() size: string = '12rem';
 
+  @HostBinding('style.--title-font-size')
+  get fontSize() {
+    return this.size;
+  }
 }
