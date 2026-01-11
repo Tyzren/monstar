5ed22951fc59820631a103abf892dd561725564e
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-01-11 13:46:03 +1100

    fe: new service to get reviews, with getting most liked reviews

---
+++ frontend/src/app/shared/services/api/get-review.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IReviewFullyPopulated } from 'app/shared/models/v2/review.schema';
+import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetReviewService {
+  private url = environment.apiV2Url;
+
+  private http = inject(HttpClient);
+
+  constructor() {}
+
+  getMostLiked(n: number = 10): Observable<Array<IReviewFullyPopulated>> {
+    const params = new HttpParams().set('n', n.toString());
+    return this.http.get<Array<IReviewFullyPopulated>>(
+      `${this.url}/reviews/popular`,
+      { params }
+    );
+  }
+}
