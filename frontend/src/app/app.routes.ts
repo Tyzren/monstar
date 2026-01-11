a907c4d5e50d37e62e241a5f4c692c99aba78e31
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-01-11 21:14:28 +1100

    fe: add user profile to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,23 +2,23 @@ import { Routes } from '@angular/router';
 import { environment } from '../environments/environment';
 
 // Component Imports
+import { AuthComponent } from '@routes/auth/auth.component';
+import { UserProfileComponent } from '@routes/user-profile/user-profile.component';
 import { AboutComponent } from './routes/about/about.component';
 import { ChangelogComponent } from './routes/changelog/changelog.component';
 import { HomeComponent } from './routes/home/home.component';
 import { NotFoundComponent } from './routes/not-found/not-found.component';
-import { ResetPasswordComponent } from './routes/reset-password/reset-password.component';
 import { SetuOverviewComponent } from './routes/setu-overview/setu-overview.component';
 import { TermsAndCondsComponent } from './routes/terms-and-conds/terms-and-conds.component';
 import { UnitListComponent } from './routes/unit-list/unit-list.component';
 import { UnitMapComponent } from './routes/unit-map/unit-map.component';
 import { UnitOverviewComponent } from './routes/unit-overview/unit-overview.component';
-import { VerifiedComponent } from './routes/verified/verified.component';
-import { AuthComponent } from '@routes/auth/auth.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'auth', component: AuthComponent },
   { path: 'list', component: UnitListComponent },
+  { path: 'user/:username', component: UserProfileComponent },
   { path: 'unit/:unitcode', component: UnitOverviewComponent },
   { path: 'map/:unitcode', component: UnitMapComponent },
   ...(environment.enableSetuCards
